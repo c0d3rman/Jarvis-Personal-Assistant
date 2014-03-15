4266d58b21124effe510d5068a2f02096f0467c4
c0d3rman <yonilern@gmail.com>
2014-03-15 12:12:12 -0700

    Backup before switching to Wit (Unstable)
    Switching to Wit AI. This is a backup.
    VERSION UNSTABLE

--- jarvis.pl
+++ jarvis.pl
@@ -5,6 +5,7 @@ use Switch;
 use IO::Select;
 use Time::HiRes qw(time);
 use File::Find;
+use Term::ANSIScreen qw(:screen :cursor);
 
 #Input setup
 my $s = IO::Select->new();
@@ -12,33 +13,51 @@ $s->add(\*STDIN);
 
 #Music setup
 my $player = new Audio::Play::MPG123;
+$player->load("Audio/Aint Nobody Got Time For That.mp3");
+#establish tpf
+my $t = time;
+$player->poll(100);
+my $tpf = (time - $t)/100;
+$player->stop();
 
 #Output setup
+#cls;
+#locate 1, 1;
 sub talk ($) {
-	print "Jarvis: $_[0]\n";
+	$stuff = shift;
+	$stuff =~ s/'/\\'/;
+	print "Jarvis: $stuff\n";
+	#$stuff =~ s/'/\\'/;
 	if ($player->state == 2) {
 		$player->pause();
 		sleep 1;
-		system("say -v Daniel '$_[0]' 2> /dev/null");
+		system("say -v Daniel '$stuff' 2> /dev/null");
 		$player->pause();
 	} else {
-		system("say -v Daniel '$_[0]' 2> /dev/null");
+		system("say -v Daniel '$stuff' 2> /dev/null");
 	}
+	#cls;
+	#locate 1, 1;
 }
 
 my $refresh = 1;
 
 while (1) {
+	$player->poll(0);
 	sleep($refresh);
 	if ($s->can_read(.5)) {
 		chomp($in = <STDIN>);
-		switch ($in) {
+		switch (lc $in) {
 			case [qw(hi hello)]  {talk("Hello, sir!");}
-			case [qw(p pause)]   {$player->state==2 ? $player->pause() : ($player->state==1 ? talk("Already paused!") : talk("No song loaded!"))}
-			case [qw(u unpause)] {$player->state==1 ? $player->pause() : ($player->state==2 ? talk("Already unpaused!") : talk("No song loaded!"))}
-			case [qw(t toggle)]  {$player->pause()}
+			case [qw(p pause)]   {$player->state==2 ? ($player->pause() and talk("Pausing song")) : ($player->state==1 ? talk("Already paused!") : talk("No song loaded!"))}
+			case [qw(u unpause)] {$player->state==1 ? ($player->pause() and talk("Unpausing song")) : ($player->state==2 ? talk("Already unpaused!") : talk("No song loaded!"))}
+			case [qw(t toggle)]  {$player->state==2 ? talk("Pausing song") : talk ("Unpausing song"); $player->pause()}
 			case [qw(s stop)]    {$player->state!=0 ? $player->stop() : talk("No song loaded!")}
 			case [qw(x exit)]    {exit}
+			case m/^say /i  {
+				$in =~ /^say (.+)/i;
+				talk($1);
+			}
 			case m/^open |^o /i  {
 				$in =~ /^open (.+)/i || $in =~ /^o (.+)/i;
 				my ($path, $filename, $rootfound) = ("", $1, 0);
@@ -58,11 +77,18 @@ while (1) {
 				find(sub {($path, $filename) = ($File::Find::name, $_) if /$filename/i}, "Audio/");
 				$filename =~ s/\....$//;
 				if ($player->load($path)) {
-					$player->pause();
 					talk("playing $filename");
-					$player->pause();
 				} else {
 					talk("could not find $filename");
+				}
+			}
+			case m/^(?:jump|skip) to  |^j /i  {
+				$in =~ /^(?:jump|skip) to (.+)/i || $in =~ /^j (.+)/i;
+				if ($player->state!=0) {
+					talk("Jumping to $1");
+					$player->jump($1/$tpf);
+				} else {
+					talk("No song loaded!");
 				}
 			}
 		}
